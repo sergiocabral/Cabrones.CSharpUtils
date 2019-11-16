b90d23834f39420d90ced1758cef27ce95fb86d3
Sergio Cabral <sergio.cabral@sapiensia.com>
2019-11-16 12:14:11 -0300

    Conversores relacionados a Guid
    Implementado métodos tipo Extension para conversão:
    - Cabrones.Utils.Converter.GuidExtensions.ToGuid
    - Cabrones.Utils.Converter.GuidExtensions.ToDatabaseText
    
    Escrito testes.

---
+++ Cabrones.Utils/Converter/GuidExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cabrones.Utils.Converter
+{
+    /// <summary>
+    /// Extensões relacionadas com: Guid
+    /// </summary>
+    public static class GuidExtensions
+    {
+        /// <summary>
+        /// Converte um valor qualquer em Guid sem disparar exception.
+        /// Para valores inválidos será usado Guid.Empty.
+        /// </summary>
+        /// <param name="value">Valor qualquer.</param>
+        /// <returns>Guid.</returns>
+        public static Guid ToGuid(this object value)
+        {
+            if (value == null || value == DBNull.Value) return Guid.Empty;
+            try
+            {
+                return Guid.Parse(value.ToString());
+            }
+            catch
+            {
+                return Guid.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Converte um Guid em valor texto para ser usado em banco de dados.
+        /// Quando receber Guid.Empty retorna DBNull.Value,
+        /// do contrário converte para texto usando Guid.ToString("D").
+        /// </summary>
+        /// <param name="guid">Guid.</param>
+        /// <returns>Valor para banco de dados.</returns>
+        public static object ToDatabaseText(this Guid guid) =>
+            guid != Guid.Empty ? 
+                (object)guid.ToString("D") : 
+                DBNull.Value;
+    }
+}
---
+++ Test.Cabrones.Utils/Converter/TestGuidExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Cabrones.Utils.Converter
+{
+    public class TestGuidExtensions
+    {
+        [Theory]
+        [InlineData(null, "00000000-0000-0000-0000-000000000000")]
+        [InlineData("inválido", "00000000-0000-0000-0000-000000000000")]
+        [InlineData("F52EEEC5-6290-4A9E-B17B-794F2D516FDA", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        [InlineData("f52eeec5-6290-4a9e-b17b-794f2d516fda", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        [InlineData("f52eeec562904a9eb17b794f2d516fda", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        [InlineData("(f52eeec5-6290-4a9e-b17b-794f2d516fda)", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        [InlineData("{f52eeec5-6290-4a9e-b17b-794f2d516fda}", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        public void deve_converter_qualquer_objeto_para_Guid_sem_disparar_exception(object valor, string guidEsperado)
+        {
+            // Arrange, Given
+            // Act, When
+
+            var guid = valor.ToGuid();
+
+            // Assert, Then
+
+            guid.ToString("D").Should().Be(guidEsperado);
+        }
+        
+        [Fact]
+        public void deve_converter_DBNullValue_para_GuidEmpty()
+        {
+            // Arrange, Given
+            // Act, When
+
+            var guid = DBNull.Value.ToGuid();
+
+            // Assert, Then
+
+            guid.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData("f52eeec5-6290-4a9e-b17b-794f2d516fda", "f52eeec5-6290-4a9e-b17b-794f2d516fda")]
+        [InlineData("00000000-0000-0000-0000-000000000000", null)]
+        public void deve_converter_um_Guid_para_texto_que_possa_ser_gravado_em_campo_de_banco_de_dados(string guid, string resultadoEsperado)
+        {
+            // Arrange, Given
+            // Act, When
+
+            var valor = Guid.Parse(guid).ToDatabaseText();
+
+            // Assert, Then
+
+            if (resultadoEsperado != null)
+            {
+                valor.Should().Be(resultadoEsperado);
+            }
+            else
+            {
+                valor.Should().Be(DBNull.Value);
+            }
+        }
+    }
+}
