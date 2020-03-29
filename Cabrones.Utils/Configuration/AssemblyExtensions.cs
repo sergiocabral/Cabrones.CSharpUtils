163a58f2598247848906abdb4dfccd0656bba851
Sergio Cabral <sergio@cabral.br.com>
2020-03-29 04:46:49 -0300

    Obtenção de FileInfo e DirectoryInfo para um Assembly.

---
+++ Cabrones.Utils/Configuration/AssemblyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Cabrones.Utils.Configuration
+{
+    /// <summary>
+    /// Funções utilitárias para Assembly. 
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Retorna o arquivo de um assembly com base na propriedade Location.
+        /// </summary>
+        /// <param name="assembly">Assembly.</param>
+        /// <returns>FileInfo</returns>
+        public static FileInfo? FileInfo(this Assembly assembly)
+        {
+            return assembly != null
+                ? new FileInfo(assembly.Location)
+                : null;
+        }
+
+        /// <summary>
+        /// Retorna o diretório de um assembly com base na propriedade Location.
+        /// </summary>
+        /// <param name="assembly">Assembly.</param>
+        /// <returns>DirectoryInfo</returns>
+        public static DirectoryInfo? DirectoryInfo(this Assembly assembly)
+        {
+            return FileInfo(assembly)?.Directory;
+        }
+    }
+}
---
+++ Test.Cabrones.Utils/Configuration/AssemblyExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace Cabrones.Utils.Configuration
+{
+    public class AssemblyExtensionsTest
+    {
+        [Fact]
+        public void deve_retornar_o_FileInfo_de_um_assembly()
+        {
+            // Arrange, Given
+
+            var assembly = Assembly.GetEntryAssembly() ?? throw new NullReferenceException();
+            var caminhoEsperado = new FileInfo(assembly.Location).FullName;
+            
+            // Act, When
+
+            var caminhoObtido = assembly.FileInfo().FullName;
+
+            // Assert, Then
+
+            caminhoObtido.Should().Be(caminhoEsperado);
+        }
+        
+        [Fact]
+        public void deve_retornar_o_DirectoryInfo_de_um_assembly()
+        {
+            // Arrange, Given
+
+            var assembly = Assembly.GetEntryAssembly() ?? throw new NullReferenceException();
+            var caminhoEsperado = new FileInfo(assembly.Location).Directory?.FullName;
+            
+            // Act, When
+
+            var caminhoObtido = assembly.DirectoryInfo().FullName;
+
+            // Assert, Then
+
+            caminhoObtido.Should().Be(caminhoEsperado);
+        }
+    }
+}
