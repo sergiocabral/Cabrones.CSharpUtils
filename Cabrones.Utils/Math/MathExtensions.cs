eab225ca02bcb320dacd0c25ae65e2f62aee503a
Sergio Cabral <sergio@cabral.br.com>
2020-05-13 08:03:04 -0300

    Método de conversão numérica ConvertTo

---
+++ Cabrones.Utils/Math/MathExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Cabrones.Utils.Math
+{
+    /// <summary>
+    ///     Funcionalidades para cálculos matemáticos.
+    /// </summary>
+    public static class MathExtensions
+    {
+        /// <summary>
+        ///     Converte um número decimal para outra base numérica.
+        /// </summary>
+        /// <param name="number">Valor na base decimal.</param>
+        /// <param name="baseChars">Caracteres da outra base numérica.</param>
+        /// <returns>Valor na outra base numérica.</returns>
+        public static string ConvertTo(this byte number, params char[] baseChars)
+        {
+            return ConvertTo((ulong) number, baseChars);
+        }
+
+        /// <summary>
+        ///     Converte um número decimal para outra base numérica.
+        /// </summary>
+        /// <param name="number">Valor na base decimal.</param>
+        /// <param name="baseChars">Caracteres da outra base numérica.</param>
+        /// <returns>Valor na outra base numérica.</returns>
+        public static string ConvertTo(this ushort number, params char[] baseChars)
+        {
+            return ConvertTo((ulong) number, baseChars);
+        }
+
+        /// <summary>
+        ///     Converte um número decimal para outra base numérica.
+        /// </summary>
+        /// <param name="number">Valor na base decimal.</param>
+        /// <param name="baseChars">Caracteres da outra base numérica.</param>
+        /// <returns>Valor na outra base numérica.</returns>
+        public static string ConvertTo(this uint number, params char[] baseChars)
+        {
+            return ConvertTo((ulong) number, baseChars);
+        }
+
+        /// <summary>
+        ///     Converte um número decimal para outra base numérica.
+        /// </summary>
+        /// <param name="number">Valor na base decimal.</param>
+        /// <param name="baseChars">Caracteres da outra base numérica.</param>
+        /// <returns>Valor na outra base numérica.</returns>
+        public static string ConvertTo(this ulong number, params char[] baseChars)
+        {
+            const int bits = sizeof(ulong) * 8;
+
+            var buffer = new char[bits];
+            var baseDigits = (uint) baseChars.Length;
+
+            var i = buffer.Length;
+            do
+            {
+                buffer[--i] = baseChars[number % baseDigits];
+                number /= baseDigits;
+            } while (number > 0);
+
+            var result = new char[bits - i];
+            Array.Copy(buffer, i, result, 0, bits - i);
+
+            return new string(result);
+        }
+    }
+}
---
+++ Test.Cabrones.Utils/Math/MathExtensionsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+// ReSharper disable ConvertToConstant.Local
+
+namespace Cabrones.Utils.Math
+{
+    public class MathExtensionsTest
+    {
+        [Theory]
+        [InlineData(3214, "abcde", "baadce")]
+        [InlineData(3214, "12345", "211435")]
+        [InlineData(3214,
+            @"!""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~", "C3")]
+        [InlineData(3214, @"¡ĊǟɁɔ&Ή˜ņ_ήƷÙȰ@¦®śǮaϫ’Ƿ£?ǬŔØ|¥cΤɣΘʣ", "ǟ’¥")]
+        public void testar_ConvertTo_convertendo_inteiro_para_bases_desconhecidas(ulong número, string baseNumérica,
+            string númeroConvertido)
+        {
+            // Arrange, Given
+            // Act, When
+
+            var númeroConvertidoObtido = número.ConvertTo(baseNumérica.ToCharArray());
+
+            // Assert, Then
+
+            númeroConvertidoObtido.Should().Be(númeroConvertido);
+        }
+
+        [Fact]
+        public void testar_ConvertTo_convertendo_inteiro_para_bases_conhecidas()
+        {
+            // Arrange, Given
+
+            var númeroULong = ulong.MaxValue;
+            var númeroUInt = uint.MaxValue;
+            var númeroUShort = ushort.MaxValue;
+            var númeroByte = byte.MaxValue;
+
+            var basesConhecidas = new[]
+            {
+                "01",
+                "01234567",
+                "0123456789",
+                "0123456789abcdef"
+            };
+
+            foreach (var basesConhecida in basesConhecidas)
+            {
+                // Act, When
+
+                var númeroULongConvertido = númeroULong.ConvertTo(basesConhecida.ToCharArray());
+                var númeroUIntConvertido = númeroUInt.ConvertTo(basesConhecida.ToCharArray());
+                var númeroUShortConvertido = númeroUShort.ConvertTo(basesConhecida.ToCharArray());
+                var númeroByteConvertido = númeroByte.ConvertTo(basesConhecida.ToCharArray());
+
+                // Assert, Then
+
+                if (basesConhecida.Length == 10)
+                {
+                    númeroULongConvertido.Should()
+                        .Be(((ulong) long.Parse(Convert.ToString((long) númeroULong, 10))).ToString());
+                    númeroUIntConvertido.Should().Be(uint.Parse(Convert.ToString(númeroUInt, 10)).ToString());
+                    númeroUShortConvertido.Should().Be(ushort.Parse(Convert.ToString(númeroUShort, 10)).ToString());
+                    númeroByteConvertido.Should().Be(byte.Parse(Convert.ToString(númeroByte, 10)).ToString());
+                }
+                else
+                {
+                    númeroULongConvertido.Should().Be(Convert.ToString((long) númeroULong, basesConhecida.Length));
+                    númeroUIntConvertido.Should().Be(Convert.ToString(númeroUInt, basesConhecida.Length));
+                    númeroUShortConvertido.Should().Be(Convert.ToString(númeroUShort, basesConhecida.Length));
+                    númeroByteConvertido.Should().Be(Convert.ToString(númeroByte, basesConhecida.Length));
+                }
+            }
+        }
+    }
+}
